6cf8e657257d05447b6fe9349b984bdcd0bd1fee
rivu <58026024+unphydra@users.noreply.github.com>
2020-01-08 17:43:24 +0530

    search for key word is added

--- convert.js
+++ convert.js
@@ -1,15 +1,27 @@
 const data = require("./pokemon.json");
-console.log(data);
 
 for (let i = 0; i < 151; i++) {
+  const pokemon = data[i];
+  let typeHtml = '<div class="backImage">';
+  pokemon.types.forEach(type => {
+    typeHtml = `${typeHtml}
+    <div class="${type}">
+    <img class="typeImg" src="./typePng/${type}.png" />
+    </div>`;
+  });
+  typeHtml = `${typeHtml}\n</div>`;
   console.log(`<div class="cards">
-  <h4>${data[i].name}</h4>
-  <div>
-  <img class="pokImage"
-    src=${data[i].art_url},
-    alt=${data[i].name}
-    />
-    </div>
-    </div>
-  `);
+  <h4>${pokemon.name}</h4>
+  <div class="contents">
+  <img
+  class="pokImage"
+  src="${pokemon.art_url}",
+  alt="${pokemon.name}"
+  />
+  <div class="description desc_text">
+  ${pokemon.description}
+  </div>
+  </div>
+  ${typeHtml}
+  </div>`);
 }
--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,17 @@
+const filterCards = function(card) {
+  return card.children[0].innerText.includes(this);
+};
+
 const searchPok = function() {
   const allCards = document.getElementsByClassName("cards");
   const cards = Array.from(allCards);
   cards.forEach(pokemon => {
-    console.log(pokemon);
-
     pokemon.style.display = "none";
+  });
+  const searchBar = document.getElementById("searchBar");
+  const text = searchBar.value;
+  const newCards = cards.filter(filterCards.bind(text));
+  newCards.forEach(pokemon => {
+    pokemon.style.display = "block";
   });
 };
